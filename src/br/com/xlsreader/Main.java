5d3298ad21aa5dbc34ade92b7f1fda1f871bda82
leonardo <leonarrangel@gmail.com>
2017-06-21 03:40:08 -0300

    finish file reader and write

--- src/br/com/xlsreader/Main.java
+++ src/br/com/xlsreader/Main.java
@@ -2,11 +2,8 @@ package br.com.xlsreader;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
@@ -14,7 +11,7 @@ import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 public class Main {
 	public static void main(String[] args) throws EncryptedDocumentException, InvalidFormatException, IOException {
 		Path path = Paths.get(System.getProperty("user.dir"));
-		File folder = new File(path.toString() + File.separator + "arquivos" + File.separator);
+		File folder = new File(path.toString() + File.separator + "entrada" + File.separator);
 		
 		Reader r = new Reader();
 
@@ -22,5 +19,6 @@ public class Main {
 			System.out.println("Lendo arquivo: " + f.getName());
 			r.exportSheet(f);
 		}
+		System.out.println("Fim de leitura.");
 	}
 }
--- src/br/com/xlsreader/Reader.java
+++ src/br/com/xlsreader/Reader.java
@@ -2,13 +2,14 @@ package br.com.xlsreader;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
@@ -29,48 +30,34 @@ public class Reader {
 	    Sheet sheet = wb.getSheetAt(0);
 	    DataFormatter formatter = new DataFormatter();
 	    
+	    Set<String> semestres = new HashSet<String>();
+	    for (Row row : sheet) {
+	    	semestres.add(formatter.formatCellValue(row.getCell(1)));
+	    }
+	    
 	    Path path = Paths.get(System.getProperty("user.dir"));
 	    
 	    String folderFileName = file.getName().split("\\.")[0];
-	    File directory = new File(Paths.get(path + File.separator + "arquivos" + File.separator + folderFileName + File.separator).toString());
+	    File directory = new File(Paths.get(path + File.separator + "processados" + File.separator + folderFileName + File.separator).toString());
+	    
+	    System.out.println("Criando diretório: " + directory.getAbsolutePath());
 	    if(!directory.exists()) directory.mkdirs();
 	    
 	    Path p;
-	    
-	    for (Row row : sheet) {
-	    	if(row.getRowNum() > 0) {
-		    	if(formatter.formatCellValue(row.getCell(6)).equals("3") || formatter.formatCellValue(row.getCell(6)).equals("6")) {
-		    		p = Paths.get(directory.getPath() + File.separator + formatter.formatCellValue(row.getCell(1)) + "_" + formatter.formatCellValue(row.getCell(4)) + "_" + formatter.formatCellValue(row.getCell(6)) + ".csv");
-		    		StringBuilder sb = new StringBuilder().append(row.getRowNum()).append(";").append(formatter.formatCellValue(row.getCell(7))).append("\n");
-		    		
-		    		Files.write(p, sb.toString().getBytes(), StandardOpenOption.APPEND, StandardOpenOption.CREATE);
-		    	}
-	    		
-//	    		System.out.println(folderFileName);
-//	    		System.out.println(row.getRowNum() + " - " + formatter.formatCellValue(row.getCell(1)) + " - " + formatter.formatCellValue(row.getCell(4)) + " - " + formatter.formatCellValue(row.getCell(6)) + " - " + formatter.formatCellValue(row.getCell(7)));
+	    for(String s : semestres) {
+	    	for (Row row : sheet) {
+	    		if(row.getRowNum() > 0) {
+			    	if((formatter.formatCellValue(row.getCell(6)).equals("3") || formatter.formatCellValue(row.getCell(6)).equals("6")) && !formatter.formatCellValue(row.getCell(1)).equals(s)) {
+			    		p = Paths.get(directory.getPath() + File.separator + "FORA_" + s + "_" + formatter.formatCellValue(row.getCell(4)).toUpperCase() + "_" + formatter.formatCellValue(row.getCell(6)) + ".csv");
+			    		StringBuilder sb = new StringBuilder().append(row.getRowNum()).append(";").append(formatter.formatCellValue(row.getCell(7))).append("\n");
+			    		
+			    		Files.write(p, sb.toString().getBytes(), StandardOpenOption.APPEND, StandardOpenOption.CREATE);
+			    	}
+	    		}
 	    	}
-
-
-//	    	if(formatter.formatCellValue(row.getCell(6)).equals("3")) {
-//	    		if(formatter.formatCellValue(row.getCell(4)).toUpperCase().equals("SUCESSO")) {
-//	    			
-//	    		} else if(formatter.formatCellValue(row.getCell(4)).toUpperCase().equals("INSUCESSO")) {
-//	    			
-//	    		}
-//	    	} else if(formatter.formatCellValue(row.getCell(6)).equals("6")) {
-//	    		if(formatter.formatCellValue(row.getCell(4)).toUpperCase().equals("SUCESSO")) {
-//	    			
-//	    		} else if(formatter.formatCellValue(row.getCell(4)).toUpperCase().equals("INSUCESSO")) {
-//	    			
-//	    		}
-//	    	}
-		}
+	    }
 	    inp.close();
 	    wb.close();
-	    
-	    FileOutputStream fileOut = new FileOutputStream("workbook.xls");
-	    wb.write(fileOut);
-	    fileOut.close();
 	}
 	
 }
